107f50ece31486eb5eec38727506ce949fc33aac
Scott <Scotthall1983@hotmail.com>
2020-06-15 08:19:44 +0100

    Added a console log which returns all the state from the mapStateToProps function. Added an onClick event which calls a function which returns the props for selecSong with the single song argument.

--- src/components/songList.js
+++ src/components/songList.js
@@ -8,7 +8,12 @@ class SongList extends React.Component {
       return (
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary">Select</button>
+            <button
+              className="ui button primary"
+              onClick={() => this.props.selectSong(song)}
+            >
+              Select
+            </button>
           </div>
           <div className="content">{song.title}</div>
         </div>
@@ -22,6 +27,7 @@ class SongList extends React.Component {
 }
 
 const mapStateToProps = state => {
+  console.log(state);
   return { songs: state.songs };
 };
 
